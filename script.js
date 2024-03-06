c8161dc10e19014ba1e5e0e2e62f9f15c340a8e9
kkasans <150230645+kkasans@users.noreply.github.com>
2024-03-06 17:48:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
     scrollBar: true,
     responsiveWidth: 400,
-    navigation: true,
+    navigation: false,
     navigationTooltips: ['home', 'about', 'skills', 'portfolio'],
     anchors: ['home', 'about', 'skills', 'portfolio'],
     menu: '#myMenu',
@@ -70,37 +70,6 @@ $(document).ready(function() {
 
     afterLoad: function ( anchorLink, index){
       var loadedSection = $(this);
-
-
-      //using index
-      if(index==1){
-        /* add opacity to arrow */
-        $('.fa-chevron-down').each(function(){
-          $(this).css('opacity','1')
-        });
-        $('.header-links a').each(function(){
-          $(this).css('color','black')
-        });
-        $('.header-links').css("background-color","transparent");
-      }
-
-      else if(index!=1){
-        $('.header-links a').each(function(){
-          $(this).css('color','black')
-        });
-        $('.header-links').css('background-color', 'black');
-      }
-
-      //using index
-      if(index == 2){
-
-        /* animate skill bars */
-        $('.skillbar').each(function(){
-          $(this).find('.skillbar-bar').animate({
-            width:jQuery(this).attr('data-percent')
-          },2500);
-        });
-      }
     }
   });
  
